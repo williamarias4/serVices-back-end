002c2c42037378f073e75b0b528559c5d5bc9eb1
William Arias <willi.am@hotmail.es>
2022-05-20 02:01:25 -0600

    Added Class config for Json Reference values.

---
+++ src/main/java/com/una/serVices/config/JsonReferenceConfig.java
@@ -0,0 +1,10 @@
+package com.una.serVices.config;
+
+public class JsonReferenceConfig {
+
+    public static final String USER_PERSON = "user-person";
+    public static final String USER_ROLE = "user-role";
+    public static final String USER_BUSINESS_PROFILE = "user-business_profile";
+    public static final String USER_JOBS_PUBLISHED = "user-jobs";
+
+}
