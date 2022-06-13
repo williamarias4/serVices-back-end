c37b5315a287869062afb61910350f0872ab9380
Fabian Gonzalez <fagm18@gmail.com>
2022-06-12 18:25:53 -0600

    Removed unneeded @Setter annotation that was causing a warning

--- src/main/java/com/una/serVices/data/JwtResponse.java
+++ src/main/java/com/una/serVices/data/JwtResponse.java
@@ -2,12 +2,10 @@ package com.una.serVices.data;
 
 import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
 
 @Data
 public class JwtResponse {
 
     @Getter
-    @Setter
     private final String jwtToken;
 }
